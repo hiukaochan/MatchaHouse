2bffef29bc78aa652058295147440026ac001493
hiukaochan <nguoidaulungsiucapvippro@gmail.com>
2025-06-30 00:35:37 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/thecodecup/Activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.thecodecup.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.Button
+import com.example.thecodecup.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val orderButton = findViewById<Button>(R.id.orderButton)
+        orderButton.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
